f05d975aa1049cd98c4196a9e4a6e51fcbda61d4
angelbedo <angelbedo104@gmail.com>
2024-01-11 09:20:45 -0500

    Apenas empezando

---
+++ src/test/java/com/valbed/valbed/ValbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valbed.valbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
